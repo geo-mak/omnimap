a861e6799769eb45c35a369dc5c7453fa36a7cd2
George Makdesi <george.makdesi@icloud.com>
2025-05-18 23:38:43 +0200

    Update benchmarking dependency.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,4 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
+
 use omnimap::OmniMap;
 use std::collections::HashMap;
 
